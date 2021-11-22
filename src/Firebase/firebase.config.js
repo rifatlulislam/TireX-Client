15e155e1fb35ae5bcfe844af59a8a0db4f940e7c
smsakawat <sakawat602872@gmail.com>
2021-11-22 11:20:24 +0600

    env added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBhOhZyXRbSniQJRe661IeikqMvCZLOsTg",
-  authDomain: "tuabike-1c417.firebaseapp.com",
-  projectId: "tuabike-1c417",
-  storageBucket: "tuabike-1c417.appspot.com",
-  messagingSenderId: "456132822589",
-  appId: "1:456132822589:web:07f4620dc50d9f12c54c54",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
